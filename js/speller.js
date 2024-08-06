4a53e42cd923e516b164a6c41d33523ed10a0852
Pavel Belokon <pbelokon@myseneca.ca>
2024-08-06 14:43:35 -0400

    check

--- js/speller.js
+++ js/speller.js
@@ -18,13 +18,38 @@ function check(inputWord) {
 	// TODO: determine if `inputWord` can be spelled
 	// with periodic table symbols; return array with
 	// them if so (empty array otherwise)
+	if (inputWord.length > 0){ 
+		for (let element of elements) { 
+			let symbol = element.symbol.toLowerCase();
+			if (symbol.length <= inputWord.length) { 
+				// did the symbol match the first
+				// one or two characters in 'inputWord' 
+				if (inputWord.slice(0, symbol.length) == symbol) { 
+					// still have characters left in inputWord ? 
+					if (inputWord.length > symbol.length) { 
+						let res = check(inputWord.slice(symbol.length))
+						// match successfully ? 
+						if (res.length > 0) { 
+							return [symbol, ...res];
+						}
+					} else { 
+						return [symbol]; 
+					}
+				}
+			}
+		}
+	}
 
 	return [];
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
-
+	for(let element of elements) { 
+		if(element.symbol.toLowerCase() == elementSymbol) { 
+			return element; 
+		}
+	}
+	
 	return {};
 }
+
