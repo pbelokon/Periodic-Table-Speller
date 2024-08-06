25d426a87e7607ccc02e96a8ae847f9a5045d853
Pavel Belokon <pbelokon@myseneca.ca>
2024-08-06 17:02:26 -0400

    one and two letter based search

--- js/speller.js
+++ js/speller.js
@@ -19,33 +19,81 @@ async function loadPeriodicTable() {
 	}
 }
 
-function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
-	if (inputWord.length > 0){ 
-		for (let element of elements) { 
-			let symbol = element.symbol.toLowerCase();
-			if (symbol.length <= inputWord.length) { 
-				// did the symbol match the first
-				// one or two characters in 'inputWord' 
-				if (inputWord.slice(0, symbol.length) == symbol) { 
-					// still have characters left in inputWord ? 
-					if (inputWord.length > symbol.length) { 
-						let res = check(inputWord.slice(symbol.length))
-						// match successfully ? 
-						if (res.length > 0) { 
-							return [symbol, ...res];
-						}
-					} else { 
-						return [symbol]; 
+function findCandidates(inputWord) { 
+	var oneLetterSymbols = []; 
+	var twoLetterSymbols = []; 
+
+	for(let i = 0; i < inputWord.length; i++) { 
+		// collect all one-letter candidates 
+		if (inputWord[i] in symbols && !oneLetterSymbols.includes(inputWord[i])) { 
+			oneLetterSymbols.push(inputWord[i]);
+		}
+
+		// collet all two-letter  candidates 
+		if(i <= (inputWord.length - 2)) { 
+			let two = inputWord.slice(i, i+2); 
+			if(two in symbols && !twoLetterSymbols.includes(two)){ 
+				twoLetterSymbols.push(two); 
+			}
+		}		
+	} 
+
+	return [...twoLetterSymbols, ...oneLetterSymbols]; 
+}
+
+function spellWord(candidates, charsLeft) { 
+
+	if(charsLeft.length == 0) { 
+		return [ ]; 
+	} else { 
+		// check for two letter symbols first 
+		if (charsLeft.length >= 2) { 
+			let two = charsLeft.slice(0, 2); 
+			let rest = charsLeft.slice(2); 
+
+			// found a match
+			if (candidates.includes(two)) {
+				// more characters too mach ?  
+				if (rest.length > 0)  { 
+					let result = spellWord(candidates, rest); 
+
+					if (result.join("") == rest) { 
+						return [two, ...result]; 
 					}
+				} 
+				else { 
+					return [two]; 
+				}
+			}
+		}
+
+		// check for one letter symbols 
+		if (charsLeft.length >= 1) { 
+			let one = charsLeft[0] ;
+			let rest = charsLeft.slice(1); 
+
+
+			if (candidates.includes(one)) { 
+				if (rest.length > 0) { 
+					let result = spellWord(candidates, rest); 
+
+					if (result.join("") == rest) { 
+						return [one, ...result];
+					}
+				} 
+				else { 
+					return [ one ];  
 				}
 			}
 		}
 	}
 
-	return [];
+	return []; 
+}
+
+function check(inputWord) {
+	var candidates = findCandidates(inputWord); 
+	return spellWord(candidates, inputWord); 
 }
 
 function lookup(elementSymbol) {
